cc9fd9c9915189abd80c6659bb08452f567c49c2
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-03-10 23:12:04 +0530

    2024_3_1)_instructor

--- src/pages/instructors/InstructorsList.js
+++ src/pages/instructors/InstructorsList.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import MaterialTable from 'material-table';
+import Button from 'react-bootstrap/Button';
 
 const InstructorsList = () => {
   return (
-    <div>InstructorsList</div>
+    <div>
+     <MaterialTable
+      title="Instructors"
+      columns={[
+        { title: 'First Name', field: 'first_name' },
+        { title: 'Last Name', field: 'last_name' },
+        { title: 'Phone', field: 'phone' },
+        { title: 'NIC', field: 'nic' },
+        { title: 'Actions', field: 'action' }
+      ]}
+      data={[
+        { first_name: 'Gimna', last_name: 'Katugampala', phone: "0764961707", nic: "20017203400",
+        action:
+        <>
+        <Button variant="primary"><i className="fas fa-pencil-alt"></i></Button>
+        </> }
+      ]}        
+      options={{
+        exportButton: true
+      }}
+    />
+    </div>
   )
 }
 
